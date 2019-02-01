9ab9ca46c922ca8d33c75e31acf10748bcca37b5
JosseWahid <jossewahud23@gmail.com>
2019-02-01 21:04:59 +0700

    Project Pertama

---
+++ src/tutorial/pkg2/TUTORIAL2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorial.pkg2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TUTORIAL2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang");
+        System.out.println("Terima Ksih");
+        System.out.print("Apa Kabar??");
+        System.out.println("Baik baik saja");
+        System.out.printf("Wiro sablen %d 212", 21213);
+        System.out.print("Dio Anjing");
+    }
+    
+}
